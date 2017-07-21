176232e4d1ecd5a84b8f064388a8e54b89ea691b
786ravikumar08 <786ravikumar08@gmail.com>
2017-07-22 02:28:28 +0530

    Add bootstrap to application

--- test/integration/pages_test.rb
+++ test/integration/pages_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class PagesTest < ActionDispatch::IntegrationTest
 
 
-test "should get home" do 
-	get pages_home_url
-	assert_response :success
+test "should get home" do
+get pages_home_url
+assert_response :success
 end
 
-test "should get home" do 
-	get root_url
-	assert_response :success
+test "should get root" do
+get root_url
+assert_response :success
 end
 
 end
